2e58b29757b2876d3e99ced6e83c0e7d887e9276
Vishnu Chennuru <vishnuchen@gmail.com>
2019-06-30 15:43:54 +0000

    working with recursion in arrays

---
+++ printing_nested_arrays.js
@@ -0,0 +1,16 @@
+function printItems(array) {
+  while (array.length > 0) {
+    const element = array.shift();
+
+    if (Array.isArray(element)) {
+      element.reverse().forEach((newElement)=> {
+        array.unshift(newElement);
+      });
+    } else {
+      console.log(element);
+    }
+  }
+}
+
+const array = ["😎", ["💩", "🤗"], "😼", "😂"];
+printItems(array);
